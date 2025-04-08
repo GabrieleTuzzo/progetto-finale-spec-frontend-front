8116c5a58e9f46dedf57d91c2fa9c385bf0f92e6
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2025-04-08 12:51:31 +0200

    FIX minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GlobalContext } from './contexts/GlobalContext';
 import useMotorcycles from './hooks/useMotorcycles';
